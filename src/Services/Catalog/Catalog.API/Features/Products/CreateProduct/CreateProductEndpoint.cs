7e660dc5d75d620ef29b656b0c1cd6f0f169953f
Milad <Milad.kardgar1@gmail.com>
2024-04-16 22:32:49 +0200

    Develope Catalog.API Microservice -Develop Feature Handler

---
+++ src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Features.Products.CreateProduct
+{
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+namespace Catalog.API.Features.Products.CreateProduct;
+
+public record CreateProductCommand(
+Guid Id,
+string Name,
+List<string> Category,
+string? Description,
+string ImageFile,
+decimal? Price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string? Description,
+    string ImageFile,
+    decimal? Price);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
