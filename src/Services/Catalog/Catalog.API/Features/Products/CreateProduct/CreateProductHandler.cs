5dd776ea4508e441308937502db283e01553ca47
Milad <Milad.kardgar1@gmail.com>
2024-04-20 16:57:43 +0200

    Implement CQRS Abstraction ICommand Into Vertical Slice Feature Handler

--- src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,15 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS.Command;
+using Catalog.API.Models;
 
 namespace Catalog.API.Features.Products.CreateProduct;
 
 public record CreateProductCommand(
-Guid Id,
+Guid? Id,
 string Name,
 List<string> Category,
 string? Description,
 string ImageFile,
-decimal? Price) : IRequest<CreateProductResult>;
+decimal? Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(
     Guid Id,
@@ -18,10 +19,31 @@ public record CreateProductResult(
     string ImageFile,
     decimal? Price);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var product = new Product
+        {
+            Id = command.Id ?? default,
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO
+        // Save to the database
+
+        return new CreateProductResult
+        (
+             product.Id,
+             product.Name,
+             product.Category,
+             product.Description,
+             product.ImageFile,
+             product.Price
+        );
     }
 }
