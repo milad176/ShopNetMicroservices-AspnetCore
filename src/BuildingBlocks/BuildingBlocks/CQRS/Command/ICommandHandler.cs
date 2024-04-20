dde0fb40261d534f4ac39470997fa163dd66b7b2
Milad <Milad.kardgar1@gmail.com>
2024-04-20 16:09:56 +0200

    Create Abstraction MediatR For CQRS-Comman-Query Seperation BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Command/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS.Command;
+
+public interface ICommandHandler<in TCommand>
+    : IRequestHandler<TCommand, Unit>
+    where TCommand : ICommand
+{
+}
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
