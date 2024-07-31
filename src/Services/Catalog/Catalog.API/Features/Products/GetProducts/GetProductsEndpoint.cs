54847f2d8a1903b946a8272068b784b16e1efec2
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2024-07-31 15:47:02 +0200

    Develop GetProducts Features Get Endpoints

---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Features.Products.GetProducts
+{
+    public static class GetProductsEndpoint
+    {
+        public static IEndpointRouteBuilder MapGetProductEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", GetProducts)
+                .WithName("GetProducts")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Products")
+                .WithDescription("Get Products");
+            return app;
+        }
+
+        private static async Task<Ok<GetProductResponse>> GetProducts(ISender sender)
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            return result.Adapt<Ok<GetProductResponse>>();
+        }
+    }
+}
