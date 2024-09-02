3ebba9248cd149b77ce9a00e3e78a96b5f8060b1
Milad <Milad.kardgar1@gmail.com>
2024-09-02 14:50:03 +0300

    CatalogAPI Develop GetProductById With CQRS

---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Features.Products.GetProductsByCategory
+{
+    public static class GetProductsByCategoryEndpoint
+    {
+        public static IEndpointRouteBuilder MapGetProductByCategoryEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", GetProductsByCategory)
+                .WithName("GetProductByCategory")
+                .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product By Category")
+                .WithDescription("Get Product By Category");
+            return app;
+        }
+
+        private static async Task<Ok<GetProductByCategoryResponse>> GetProductsByCategory(string category, ISender sender)
+        {
+            var query = new GetProductByCategoryQuery(category!);
+            var queryResult = await sender.Send(query).ConfigureAwait(false);
+            var result = queryResult.Adapt<GetProductByCategoryResponse>();
+            return TypedResults.Ok(result);
+        }
+    }
+}
