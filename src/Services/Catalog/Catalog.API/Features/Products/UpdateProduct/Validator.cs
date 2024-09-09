3a10dc8ecf21984cf9ee68434b7e7f433fa8ad31
Milad <Milad.kardgar1@gmail.com>
2024-09-09 15:56:15 +0300

    Add Validator for CRUD with Fluent Validation

---
+++ src/Services/Catalog/Catalog.API/Features/Products/UpdateProduct/Validator.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Features.Products.UpdateProduct;
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+
+        When(x => x.Category is not null,
+            () => RuleForEach(x => x.Category)
+            .NotEmpty()
+            .WithMessage("Category item cannot be null"));
+
+        RuleFor(x => x.ImageFile)
+            .NotEmpty()
+            .WithMessage("ImageFile cannot be null")
+            .When(x => x.ImageFile is not null);
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0)
+            .WithMessage("Price must be greater than 0")
+            .When(x => x.Price is not null);
+    }
+}
