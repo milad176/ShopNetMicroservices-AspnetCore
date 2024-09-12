045d5ba4fe9e48978d0ba8550b8aef29b1f15f94
Milad <Milad.kardgar1@gmail.com>
2024-09-12 11:29:13 +0300

    Global Exception With IExceptionHandler

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildingBlocks.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+    }
+
+    public BadRequestException(string message, string details) : base(message)
+    {
+        Details = details;
+    }
+
+    public string? Details { get; }
+}
+
+public record InvalidPropertyResponse(string PropertyName, string Reason);
