d8aaf106396a18a7cd6816c2dfec9c0c8a66e558
Milad <Milad.kardgar1@gmail.com>
2024-09-23 10:29:21 +0300

    Add health check for catalog Microservices

---
+++ src/Services/Catalog/Catalog.API/Common/HealthChecksExtension.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.HealthChecks;
+
+namespace Catalog.API.Common;
+
+public static class HealthChecksExtension
+{
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDefaultHealthChecks(configuration);
+
+        services.AddHealthChecks()
+            .AddNpgSql(configuration.GetConnectionString("Database")!, name: "postgres", tags: ["ready", "liveness"]);
+
+        return services;
+    }
+}
