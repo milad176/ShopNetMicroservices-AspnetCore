f69ecd060d2a51cca20393da61bee222b63efc1d
Milad <Milad.kardgar1@gmail.com>
2024-10-28 16:03:01 +0200

    Create Domain Models for Basket API Microservices

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = [];
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        // Required for Mapping
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+    }
+}
