be92832de53ca9be886ff73af15da1541af2bcad
Milad <Milad.kardgar1@gmail.com>
2024-10-29 15:46:45 +0200

    Develop StoreBasket Feature Endpoint Class With CQRS and MediatR

---
+++ src/Services/Basket/Basket.API/Features/Basket/StoreBasket/Models.cs
@@ -0,0 +1,5 @@
+﻿namespace Basket.API.Features.Basket.StoreBasket
+{
+    public record StoreBasketResponse(ShoppingCart ShoppingCart);
+    public record StoreBasketRequest(ShoppingCart ShoppingCart);
+}
--- src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,29 @@
-﻿namespace Basket.API.Features.Basket.StoreBasket
+﻿using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Basket.API.Features.Basket.StoreBasket;
+
+public static class StoreBasketEndpoint
 {
-    public class StoreBasketEndpoint
+    public static IEndpointRouteBuilder MapStoreBasketEndpoint(this IEndpointRouteBuilder app)
     {
+        app.MapPost("/", StoreBasketAsync)
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Store Basket")
+            .WithDescription("Store Basket");
+
+        return app;
+    }
+
+    private static async Task<Ok<StoreBasketResponse>> StoreBasketAsync(StoreBasketRequest request, ISender sender)
+    {
+        var command = request.Adapt<StoreBasketCommand>();
+        var result = await sender.Send(command).ConfigureAwait(false);
+        var response = result.Adapt<StoreBasketResponse>();
+        return TypedResults.Ok(response);
     }
 }
