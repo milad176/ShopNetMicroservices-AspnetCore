76f94afa303377105b0f6814be532651cd1dfce5
Milad <Milad.kardgar1@gmail.com>
2024-10-29 15:06:21 +0200

    Develop StoreBasket Feature Handler Class With CQRS and MediatR

--- src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Basket.API.Features.Basket.StoreBasket
+﻿using BuildingBlocks.CQRS.Command;
+
+namespace Basket.API.Features.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart? ShoppingCart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(ShoppingCart ShoppingCart);
+
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
+        public Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
