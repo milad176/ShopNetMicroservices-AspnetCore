08cd18debaaebf9e7441d910853deef7c8dd4d11
Milad <Milad.kardgar1@gmail.com>
2024-11-11 15:53:24 +0200

    Develop CacheBasketRepository with Proxy Pattern, Decorator Pattern and Scrutor

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string username, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(username, cancellationToken).ConfigureAwait(false);
+        if (!string.IsNullOrEmpty(cachedBasket))
+            return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+        var basketInDb = await repository.GetBasketAsync(username, cancellationToken).ConfigureAwait(false);
+        await cache.SetStringAsync(username, JsonSerializer.Serialize(basketInDb), cancellationToken);
+        return basketInDb;
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasketAsync(basket, cancellationToken).ConfigureAwait(false);
+
+        await cache.SetStringAsync(basket.Username, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasketAsync(string username, CancellationToken cancellationToken = default)
+    {
+        var isSuccess = await repository.DeleteBasketAsync(username, cancellationToken).ConfigureAwait(false);
+
+        await cache.RemoveAsync(username, cancellationToken).ConfigureAwait(false);
+
+        return isSuccess;
+    }
+}
--- src/Services/Basket/Basket.API/Data/IBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -2,7 +2,7 @@
 
 public interface IBasketRepository
 {
-    Task<ShoppingCart> GetBasketAsync(string UserName, CancellationToken cancellationToken = default);
+    Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken = default);
     Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken cancellationToken = default);
-    Task<bool> DeleteBasketAsync(string UserName, CancellationToken cancellationToken = default);
+    Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken = default);
 }
