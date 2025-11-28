580a2d0b61a282a5ba137f99ce88a894c1ef3170
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2025-11-28 15:48:53 +0200

    Add and calculate shopping cart items discount in Basket Microservice.

--- src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,16 +1,34 @@
 ﻿using BuildingBlocks.CQRS.Command;
+using Discount.Grpc.Protos;
 
 namespace Basket.API.Features.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart? ShoppingCart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(ShoppingCart ShoppingCart);
 
-    public class StoreBasketCommandHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    public class StoreBasketCommandHandler(
+        IBasketRepository basketRepository,
+        DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
-            var result = await basketRepository.StoreBasketAsync(command.ShoppingCart!, cancellationToken).ConfigureAwait(false);
+            await DeductDiscount(command.ShoppingCart, cancellationToken);
+
+            var result = await basketRepository.StoreBasketAsync(command.ShoppingCart!, cancellationToken)
+                .ConfigureAwait(false);
             return new StoreBasketResult(result);
+        }
+
+        private async Task DeductDiscount( ShoppingCart  shoppingCart , CancellationToken cancellationToken)
+        {
+            foreach (var item in  shoppingCart.Items)
+            {
+                var coupon = await discountProtoService.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName },
+                    cancellationToken: cancellationToken);
+
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
