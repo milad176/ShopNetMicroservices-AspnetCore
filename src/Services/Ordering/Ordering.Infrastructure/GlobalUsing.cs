447b728ba3d1acd7c3a80f4ce31d9fec060c91e3
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-01-28 13:18:35 +0200

    Develop SaveChanges Interceptors to Auditing Entities for Order Entities.

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -2,3 +2,5 @@ global using Microsoft.EntityFrameworkCore;
 global using Ordering.Infrastructure.Data;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObject;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Infrastructure.Data.Interceptors;
