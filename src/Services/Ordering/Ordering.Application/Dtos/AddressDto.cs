345359493a6a415119b721ebb8d90d9b89b765a0
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-02-13 11:39:33 +0200

    Develop DTO Classes for Oders in Ordering.Application Layer.

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,10 @@
+namespace Ordering.Application.Dtos;
+
+public record AddressDto(
+    string FirstName,
+    string LastName,
+    string EmailAddress,
+    string AddressLine,
+    string Country,
+    string State,
+    string ZipCode);
