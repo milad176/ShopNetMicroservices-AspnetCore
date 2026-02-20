0fd1b521f91f060ae68a84c856dcb984b16930e0
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-02-20 13:41:34 +0200

    Develop GetOrdersByCustomerQuery with CQRS and MediatR for Ordering.App

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,5 @@
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomerResult>;
+
+public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
