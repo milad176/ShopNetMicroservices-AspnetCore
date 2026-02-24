36849982e8fa95253381ca1f54e7f80adbcb637d
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-02-24 10:06:13 +0200

    Develop CreateOrder Endpoints for REST APIs in Ordering.API Layer.

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,3 +1,5 @@
+using Ordering.Application.Orders.Commands.CreateOrder;
+
 namespace Ordering.API.Endpoints;
 
 //- Accepts a CreateOrderRequest object.
@@ -5,6 +7,28 @@ namespace Ordering.API.Endpoints;
 //- Uses MediatR to send the command to the corresponding handler.
 //- Returns a response with the created order's ID.
 
-public class CreateOrder
+public record CreateOrderRequest(OrderDto Order);
+
+public record CreateOrderResponse(Guid Id);
+
+public class CreateOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateOrderResponse>();
+
+                return Results.Created($"/orders/{response.Id}", response);
+            })
+            .WithName("CreateOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order");
+    }
 }
---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,4 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
