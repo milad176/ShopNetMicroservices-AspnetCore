5a5d77a5662d0d0bc4feb2b7fb4d2ef99ee8a1e5
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-02-24 10:15:31 +0200

    Develop UpdateOrder Endpoints for REST APIs in Ordering.API Layer.

--- src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -1,3 +1,5 @@
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
 namespace Ordering.API.Endpoints;
 
 //- Accepts a UpdateOrderRequest.
@@ -5,6 +7,27 @@ namespace Ordering.API.Endpoints;
 //- Sends the command for processing.
 //- Returns a success or error response based on the outcome.
 
-public class UpdateOrder
+public record UpdateOrderRequest(OrderDto Order);
+public record UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateOrder")
+            .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Order")
+            .WithDescription("Update Order");
+    }
 }
