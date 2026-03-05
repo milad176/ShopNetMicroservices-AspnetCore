c3435975714a9d3a690fd2d4b8246954f2da5db4
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-05 10:52:31 +0200

    Develop BasketCheckout Event in BuildingBlocks.Messaging Class Library.

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+namespace BuildingBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
