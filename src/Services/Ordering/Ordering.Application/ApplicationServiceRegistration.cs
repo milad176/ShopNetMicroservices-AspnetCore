f838b9ff9a0460e57e46f8ad70aad95884473a25
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-06 11:12:06 +0200

    Add BB.Messaging Library Refrence to Ordering.Application.csproj and Register MassTransit RabbitMQ to Ordering.Application DI.

--- src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -1,14 +1,16 @@
 using System.Reflection;
-using BuildingBlocks.CQRS.Extensions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 
 public static class ApplicationServiceRegistration
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.RegisterMediateR(Assembly.GetExecutingAssembly());
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
         return services;
     }
