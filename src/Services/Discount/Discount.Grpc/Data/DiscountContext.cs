5c1433ab8bdc83060d36e161c19878e960896f27
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-10 10:06:44 +0200

    DiscountContext Update.

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -14,8 +14,8 @@ public class DiscountContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Coupon>().HasData(
-            new Coupon { Id = 1, ProductName = "Ïphone X", Description = "Ïphone Description", Amount = 200 },
-            new Coupon { Id = 2, ProductName = "Samsung", Description = "Samsung Description", Amount = 150 }
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "Iphone Description", Amount = 200 },
+            new Coupon { Id = 2, ProductName = "Samsung 22", Description = "Samsung Description", Amount = 150 }
         );
     }
 }
