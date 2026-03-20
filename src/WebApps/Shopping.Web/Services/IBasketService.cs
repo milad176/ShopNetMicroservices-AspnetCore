a0a825c5de2f32e090434c720ff7921ea0f1bf97
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-20 11:16:39 +0200

    develop service classes to cosume YarpApiGatewaye.

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,19 @@
+using Refit;
+using Shopping.Web.Models.Basket;
+
+namespace Shopping.Web.Services;
+
+public interface IBasketService
+{
+    [Get("/basket-service/api/v1/basket/{userName}")]
+    Task<GetBasketResponse> GetBasket(string userName);
+
+    [Post("/basket-service/api/v1/basket")]
+    Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+    [Delete("/basket-service/api/v1/basket/{userName}")]
+    Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+    [Post("/basket-service/api/v1/basket/checkout")]
+    Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+}
