accb89e6b72326c020e879f7e3e23a466177802c
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-23 12:19:24 +0200

    develop index html pages and cs files with shared partial product pages.

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -6,6 +6,8 @@ namespace Shopping.Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    
+    public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();    
 
     public IndexModel(ILogger<IndexModel> logger)
     {
