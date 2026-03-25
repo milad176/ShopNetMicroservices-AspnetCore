acc9af0fac7b6bb8d60d37e4dcb08627622bd7e2
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-25 10:54:26 +0200

    add to cart submit handle in Index.cshtml page with IBasketService.

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 namespace Shopping.Web.Pages;
 
-public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger) : PageModel
+public class IndexModel(ICatalogService catalogService, IBasketService basketService, ILogger<IndexModel> logger)
+    : PageModel
 {
     public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
 
@@ -11,5 +12,27 @@ public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logg
         //var result = await catalogService.GetProducts(2, 3);
         ProductList = result.Product.Data;
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Add to cart button clicked");
+
+        var productResponse = await catalogService.GetProduct(productId);
+
+        var basket = await basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
+        {
+            ProductId = productId,
+            ProductName = productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
+
+        await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+        return RedirectToPage("Cart");
     }
 }
