f6bdba9d62548cdec35d1203db74328896035dcb
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-03-26 12:56:01 +0200

    develop ProductList razer page in shoping web application.

---
+++ src/WebApps/Shopping.Web/Pages/ProductList.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Web.Pages;
+
+public class ProductListModel(
+    ICatalogService catalogService,
+    IBasketService basketService,
+    ILogger<ProductListModel> logger) : PageModel
+{
+    public IEnumerable<string> CategoryList { get; set; } = [];
+    public IEnumerable<ProductModel> ProductList { get; set; } = [];
+    
+    [BindProperty(SupportsGet = true)]
+    public string SelectedCategory { get; set; } = default!;
+
+
+    public async Task<IActionResult> OnGetAsync(string categoryName)
+    {
+        var response = await catalogService.GetProducts();
+
+        CategoryList = response.Product.Data.SelectMany(p => p.Category).Distinct();
+
+        if (!string.IsNullOrWhiteSpace(categoryName))
+        {
+            ProductList = response.Product.Data.Where(p => p.Category.Contains(categoryName));
+            SelectedCategory = categoryName;
+        }
+        else
+        {
+            ProductList = response.Product.Data;
+        }
+
+        return Page();
+    }
+    
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Add to cart button clicked");
+        var productResponse = await catalogService.GetProduct(productId);
+
+        var basket = await basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
+        {
+            ProductId = productId,
+            ProductName = productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
+
+        await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+        return RedirectToPage("Cart");
+    }
+}
