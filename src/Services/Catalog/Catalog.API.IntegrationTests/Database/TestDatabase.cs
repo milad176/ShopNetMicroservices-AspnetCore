2954f2f024ad16863b4e7afcb52345e4a2d5d185
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-07-21 10:39:22 +0300

    Implement test database for integration test for catalog service.

--- src/Services/Catalog/Catalog.API.IntegrationTests/Database/TestDatabase.cs
+++ src/Services/Catalog/Catalog.API.IntegrationTests/Database/TestDatabase.cs
@@ -1,6 +1,25 @@
+using Testcontainers.PostgreSql;
+
 namespace Catalog.API.IntegrationTests.Database;
 
-public class TestDatabase
+public sealed class TestDatabase
 {
-    
+    private readonly PostgreSqlContainer _container =
+        new PostgreSqlBuilder()
+            .WithDatabase("CatalogDb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+    public string ConnectionString => _container.GetConnectionString();
+
+    public async Task StartAsync()
+    {
+        await _container.StartAsync();
+    }
+
+    public async Task StopAsync()
+    {
+        await _container.StopAsync();
+    }
 }
