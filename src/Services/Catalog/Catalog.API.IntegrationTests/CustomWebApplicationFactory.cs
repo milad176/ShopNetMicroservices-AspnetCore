9a2d467d78b02581c17b3281007b143d1dfbb1dd
Milad Kardgar <40167363+milad176@users.noreply.github.com>
2026-07-24 09:04:52 +0300

    implement WebApplicationFactory for integration testing for catalog service.

--- src/Services/Catalog/Catalog.API.IntegrationTests/CustomWebApplicationFactory.cs
+++ src/Services/Catalog/Catalog.API.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,8 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 
 namespace Catalog.API.IntegrationTests;
 
-public class CustomWebApplicationFactory: WebApplicationFactory<Program>
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-    
+    private readonly string _connectionString;
+
+    public CustomWebApplicationFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:Database"] = _connectionString
+            });
+        });
+    }
 }
